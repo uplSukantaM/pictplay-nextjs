0933b3603791345c9853dfef44796483c7a84019
uplsukantam <sukanta.majhi@utplco.com>
2022-01-31 11:14:02 +0530

    Login screen done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
