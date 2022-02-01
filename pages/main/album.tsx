822ae2c66d2da7b30a09dd0a2ba5fdf4cbbd2220
uplNiladri <niladri.barui@utplco.com>
2022-02-01 15:31:09 +0530

    media preview api call done

--- pages/main/album.tsx
+++ pages/main/album.tsx
@@ -5,29 +5,35 @@ import Image from "next/image";
 
 export default function Example() {
   const history = useRouter();
-  const [userDetails, setUSerDetails] = useState({})
-  const [albums, setAlbums] = useState([])
+  const [userDetails, setUSerDetails] = useState({});
+  const [albums, setAlbums] = useState([]);
+  const goToMedia = (data: any) => {
+    let userId = userDetails?._id;
+    history.push({
+      pathname: "/main/mediaPreview",
+      search: `?userId=${userId}&albumId=${data?._id}`,
+    });
+  };
   useEffect(() => {
     let userDetails = JSON.parse(localStorage.getItem("user"));
-    setUSerDetails(userDetails)
-    const auth = localStorage.getItem('token')
+    setUSerDetails(userDetails);
+    const auth = localStorage.getItem("token");
     // console.log(userDetails._id);
   }, []);
   useEffect(() => {
     getAlbumList();
   }, [userDetails]);
   const returnCount = (data: any) => {
-    let count = 0
+    let count = 0;
     if (data?.imageCount || data?.videoCount) {
-      count = data?.imageCount + data?.videoCount
+      count = data?.imageCount + data?.videoCount;
     }
-    return count
-  }
+    return count;
+  };
 
   const getAlbumList = () => {
-
     console.log("album api call album index");
-    const auth = localStorage.getItem('token')
+    const auth = localStorage.getItem("token");
     const reqstValues: object = {
       method: "GET",
       headers: {
@@ -35,7 +41,10 @@ export default function Example() {
         authorization: auth,
       },
     };
-    fetch(`${process.env.NEXT_PUBLIC_MEDIA_API_URL}/albums/${userDetails?._id}`, reqstValues)
+    fetch(
+      `${process.env.NEXT_PUBLIC_MEDIA_API_URL}/albums/${userDetails?._id}`,
+      reqstValues
+    )
       .then((result) => {
         if (result.status === 401) {
           history.push("/");
@@ -46,7 +55,7 @@ export default function Example() {
       .then((result) => {
         if (!result.error) {
           console.log(result);
-          setAlbums(result.result)
+          setAlbums(result.result);
         }
       })
       .catch((err: string) => {
@@ -59,31 +68,42 @@ export default function Example() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            {albums && albums.length > 0 && albums.map((item: any, index: number) => {
-              return (
-                <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={index}>
-                  <a className="block relative h-48 rounded overflow-hidden">
-                    <Image
-                      alt="ecommerce"
-                      className="object-cover object-center w-full h-full block"
-                      src={`${process.env.NEXT_PUBLIC_MEDIA_API_URL}/uploads/?key=${item?.thumbnail?.key}&width=300&fit=cover`}
-                      layout="fill"
-                      quality={100}
-                      placeholder="blur"
-                      blurDataURL="/DefaultAlbum.svg"
-                      loading="lazy"
-                    />
-                  </a>
-                  <div className="mt-4">
-                    <h2 className="text-gray-900 title-font text-lg font-medium">
-                      {item?.name}
-                    </h2>
-                    <p className="mt-1">{returnCount(item)} items</p>
+            {albums &&
+              albums.length > 0 &&
+              albums.map((item: any, index: number) => {
+                return (
+                  <div
+                    className="lg:w-1/4 md:w-1/2 p-4 w-full"
+                    key={index}
+                    onClick={() => {
+                      goToMedia(item);
+                    }}
+                  >
+                    <a className="block relative h-48 rounded overflow-hidden">
+                      <Image
+                        alt="ecommerce"
+                        className="object-cover object-center w-full h-full block"
+                        src={
+                          item?.thumbnail
+                            ? `${process.env.NEXT_PUBLIC_MEDIA_API_URL}/uploads/?key=${item?.thumbnail?.key}&width=300&fit=cover`
+                            : "/DefaultAlbum.svg"
+                        }
+                        layout="fill"
+                        quality={100}
+                        placeholder="blur"
+                        blurDataURL="/DefaultAlbum.svg"
+                        loading="lazy"
+                      />
+                    </a>
+                    <div className="mt-4">
+                      <h2 className="text-gray-900 title-font text-lg font-medium">
+                        {item?.name}
+                      </h2>
+                      <p className="mt-1">{returnCount(item)} items</p>
+                    </div>
                   </div>
-                </div>
-              )
-            })}
-
+                );
+              })}
           </div>
         </div>
       </section>
---
+++ pages/main/mediaPreview.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import styles from "../../styles/media.module.css";
+
+function mediaPreview() {
+  const getQueryStringValue = (key: string) => {
+    return decodeURIComponent(
+      window.location.search.replace(
+        new RegExp(
+          "^(?:.*[&\\?]" +
+            encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") +
+            "(?:\\=([^&]*))?)?.*$",
+          "i"
+        ),
+        "$1"
+      )
+    );
+  };
+  const albumId = getQueryStringValue("albumId");
+  const MATCH_URL_IMAGE = /\/(jpg|jpeg|png)($|\?)/i;
+  const [imagePath, setImagePath] = useState([]);
+  const getMedia = () => {
+  
+    console.log(albumId);
+
+    const reqstValues: object = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: localStorage.getItem("token"),
+      },
+    };
+    fetch(
+      `${process.env.NEXT_PUBLIC_MEDIA_API_URL}/medias/${albumId}?dataPerPage=300`,
+      reqstValues
+    )
+      .then((result) => result.json())
+      .then((result) => {
+        if (!result.error) {
+          // dispatch(mediaList(result.result));
+          setImagePath(result.result);
+          console.log(result);
+        }
+      })
+      .catch((err: string) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getMedia();
+  }, [albumId]);
+
+  return (
+    <div>
+      <div className={styles.displayImage}>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 3, 900: 4 }}>
+          <Masonry columnsCount={5}>
+            {imagePath &&
+              imagePath.length > 0 &&
+              imagePath.map((image: any, index: number) => {
+                return (
+                  <div className={styles.masonryImage} key={index}>
+                    <Image
+                      src={
+                        image?.mediaFile?.mimetype.includes("image") &&
+                        `${process.env.NEXT_PUBLIC_MEDIA_API_URL}/uploads/?key=${image?.mediaFile?.key}&height=auto&width=200&fit=contain`
+                      }
+                      alt="media"
+                      loading="lazy"
+                      layout="fill"
+                      placeholder="blur"
+                      blurDataURL="/DefaultAlbum.svg"
+                    />
+                  </div>
+                );
+              })}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+    </div>
+  );
+}
+
+export default mediaPreview;
